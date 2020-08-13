753b2da1261fbcc0e54f2db4f5e2e5b13529e994
Bourhano <bourhan.dernayka@gmail.com>
2020-08-13 10:56:29 +0000

    add 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c != 'z'+1)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
