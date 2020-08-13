ad84357f4ea8c4f692145a71fc938a7535fec153
Bourhano <bourhan.dernayka@gmail.com>
2020-08-13 10:18:31 +0000

    add new directory 0x02 containing header file and exercice 1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *holberton = "Holberton\n";
+	int i;
+
+	/*
+	l = sizeof(holberton);
+	for(i = 0;i < l; i++)
+		_putchar(*(holberton+i));
+	*/
+	i=0;
+	while (holberton[i] != '\0')
+	{
+		_putchar(*(holberton+i));
+		i++;
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is the third directory of the C learning repo, on a UNIX-like system.
+
+It contains exercices about functions and nesting loops.
