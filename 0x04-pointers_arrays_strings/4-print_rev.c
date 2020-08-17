27ee1ac8758b1ae1d7698437e7f9df63b4c9433b
Bourhano <bourhan.dernayka@gmail.com>
2020-08-17 21:31:54 +0000

    fix 4

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,8 @@
 /**
  * print_rev - Prints the inverse of the input string to stdout
  * @s: the string to be printed
+ *
+ * Return: none
  **/
 void print_rev(char *s)
 {
@@ -10,11 +12,7 @@ void print_rev(char *s)
 
 	while (*(s + l) != '\0')
 		l++;
-	l--;
 	while (l > 0)
-	{
-		_putchar(*(s + l));
-		l--;
-	}
+		_putchar(*(s + --l));
 	_putchar('\n');
 }
