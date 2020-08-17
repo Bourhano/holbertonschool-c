b0c4c296f95be78fbeeb629cb71b852fead79f25
Bourhano <bourhan.dernayka@gmail.com>
2020-08-17 22:18:58 +0000

    fix 5,7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -3,14 +3,14 @@
 /**
  * puts_half - Prints the second half of the input string to stdout
  * @str: the string to be printed
-*/
+ **/
 void puts_half(char *str)
 {
 	int l = 0, i;
 
 	while (*(str + l) != '\0')
 		l++;
-	for (i = (l % 1 == 0) ? l / 2 : l / 2 + 1; i < l; i++)
+	for (i = (l % 1 == 0) ? l / 2 : (l + 1) / 2; i < l; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 }
