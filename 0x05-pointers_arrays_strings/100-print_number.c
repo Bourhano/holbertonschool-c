002e5d3397fbe8fb024569a36c8acf12b92b7433
Bourhano <bourhan.dernayka@gmail.com>
2020-08-20 09:25:40 +0000

    hot fix 100

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -8,7 +8,21 @@ void print_number(int n)
 {
 	int l = 0, i, exn = n, rm, print, cnt, j;
 
-	if (n < 0)
+	if (n == -2147483647)
+	{
+		_putchar('-');
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('7');
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		print_number(-n);
