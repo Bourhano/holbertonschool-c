73410ab2ad9e7f3835a18d99a7eedb7b99023e23
Bourhano <bourhan.dernayka@gmail.com>
2020-08-20 09:12:48 +0000

    fix 100, no arrays

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -6,8 +6,7 @@
  **/
 void print_number(int n)
 {
-	int l = 0, i;
-	char nbc[50];
+	int l = 0, i, exn = n, rm, print, cnt, j;
 
 	if (n < 0)
 	{
@@ -19,11 +18,16 @@ void print_number(int n)
 	else
 	{
 		for ( ; n > 0 ; l++)
-		{
-			nbc[l] = n % 10;
 			n /= 10;
+		for (i = l; i > 0; i--)
+		{
+			cnt = 1;
+			for (j = 1 ; j < i; j++)
+				cnt *= 10;
+			print = exn / cnt;
+			rm = print * cnt;
+			exn -= rm;
+			_putchar('0' + print);
 		}
-		for (i = l - 1; i >= 0; i--)
-			_putchar('0' + *(nbc + i));
 	}
 }
