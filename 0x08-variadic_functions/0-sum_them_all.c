74544fe1e3ad6efa734bf2b5569ad7134adfc341
Bourhano <bourhan.dernayka@gmail.com>
2020-08-25 15:57:17 +0000

    betty task 1

--- 0x08-variadic_functions/0-sum_them_all.c
+++ 0x08-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
+	if (!n)
+		return (0);
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
