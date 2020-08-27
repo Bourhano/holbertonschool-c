e5dfed0222d760b34fa2b3aeff617083714645bd
Bourhano <bourhan.dernayka@gmail.com>
2020-08-27 20:57:47 +0000

    add task 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all args
+ * @ac: arg count
+ * @av: arguments values
+ *
+ * Return: pointer to the cat string
+ **/
+char *argstostr(int ac, char **av)
+{
+	int i = 0;
+
+	if ( ac == 0  || av == NULL)
+		return (NULL);
+}
