7adfcaa51820da5b012c337e603fae538b038e5f
Bourhano <bourhan.dernayka@gmail.com>
2020-08-27 20:51:21 +0000

    unuse calloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -9,12 +9,15 @@
  **/
 int **alloc_grid(int width, int height)
 {
-	int **grid, i = 0;
+	int **grid, i = 0, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(height * sizeof(int));
+	grid = (int **)malloc(height, sizeof(int));
 	for (i = 0; i < height; i++)
-		grid[i] = (int *)calloc(width, sizeof(int));
+		grid[i] = (int *)malloc(width, sizeof(int));
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	return (grid);
 }
