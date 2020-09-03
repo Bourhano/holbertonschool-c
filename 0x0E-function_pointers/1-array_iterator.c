25432a82bf5c5ff08310f3bdced3dac612948008
Bourhano <bourhan.dernayka@gmail.com>
2020-09-03 20:58:48 +0000

    fix 1, add 2

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -9,8 +9,9 @@
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        size_t i;
+	size_t i;
 
-	for (i = 0; i < size; i++)
-		(*action)(array[i]);
+	if (size != 0 && action != 0)
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
 }
---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array of int elements
+ * @size: size of the array
+ * @cmp: function pointer that sets the comparison criteria
+ *
+ * Return:  index of the first element for which the cmp function
+ * does not return 0
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
