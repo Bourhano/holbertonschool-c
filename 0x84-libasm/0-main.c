78290540e35ac7d9a882a0a283ae1f7689f1c1c7
Bourhano <bourhan.dernayka@gmail.com>
2020-12-14 19:19:11 +0000

    strlen in assembly, test with Makefile

---
+++ 0x84-libasm/0-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libasm.h"
+
+#define S1  "Holberton School"
+#define S2  ""
+#define S3  "\0"
+
+/**
+ * main - Program entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	assert(strlen(S1) == 16);
+	assert(strlen(S2) == 0);
+	assert(strlen(S3) == 0);
+
+	printf("All good!\n");
+	return (EXIT_SUCCESS);
+}
