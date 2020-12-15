0eb00632965b43ff330a9064d116bee01116092b
Bourhano <bourhan.dernayka@gmail.com>
2020-12-15 19:25:42 +0000

    implement strpbrk, update Makefile

---
+++ 0x84-libasm/12-strpbrk.asm
@@ -0,0 +1,47 @@
+bits 64
+section .text
+	[GLOBAL asm_strpbrk:] ; C function
+
+asm_strpbrk:
+	push rcx
+	push rdx
+	push r8
+	push r9
+	xor r8, r8
+	xor r9, r9
+	xor rcx, rcx
+
+	.compare_char:
+	cmp byte [rdi+rcx], 0
+	jz .not_found		; s[i] == 0, meaning no byte of accept could be found
+	xor rdx, rdx
+	mov r8b, byte [rdi+rcx]
+
+	.compare_with_reject:
+	mov r9b, byte [rsi+rdx]
+	cmp r9b, 0
+	jz .advance_one_char	; No byte could be found
+	cmp r8b, r9b
+	je .found		; One byte found
+	inc rdx
+	jmp .compare_with_reject
+
+	.advance_one_char:
+	inc rcx
+	jmp .compare_char
+
+	.not_found:
+	xor rax, rax		; Return NULL
+	jmp .ret
+
+	.found:
+	mov rax, rdi
+	add rax, rcx		; Return s + i
+	jmp .ret
+
+	.ret:
+	pop r9
+	pop r8
+	pop rdx
+	pop rcx
+	ret
